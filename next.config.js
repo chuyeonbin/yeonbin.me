b5fdd6f34c5aacf89ca04a48c126ee2487aa373b
ChuYeonBin <cndusqls98@gmail.com>
2023-09-13 14:53:12 +0900

    chore: next-contentlayer 플러그인 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withContentlayer } = require('next-contentlayer');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+module.exports = withContentlayer((phase) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return nextConfig;
+  }
+
+  return nextConfig;
+});
