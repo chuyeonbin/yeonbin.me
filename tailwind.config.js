a2ad5b5ed7ba7a3a06128adea5b8a42fc48756cc
ChuYeonBin <cndusqls98@gmail.com>
2023-09-13 23:21:10 +0900

    chore: tailwind.config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./app/**/*.tsx', './components/**/*.tsx'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
