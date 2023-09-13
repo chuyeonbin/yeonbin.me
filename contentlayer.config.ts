2a84e9d325adf974a40b6e9e45d2fa8f92867e45
ChuYeonBin <cndusqls98@gmail.com>
2023-09-13 14:51:26 +0900

    contentlayer.config 수정

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,15 +3,24 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 export const Post = defineDocumentType(() => ({
   name: 'Post',
   contentType: 'mdx',
-  filePathPattern: `**/*.mdx`,
+  filePathPattern: `*.mdx`,
   fields: {
     title: { type: 'string', required: true },
-    date: { type: 'string', required: true },
+    publishAt: { type: 'date', required: true },
     description: { type: 'string', required: true },
+    tags: {
+      type: 'list',
+      of: { type: 'string' },
+      required: true,
+      default: [],
+    },
+  },
+  computedFields: {
+    slug: { type: 'string', resolve: (post) => `/${post._raw.flattenedPath}` },
   },
 }));
 
 export default makeSource({
-  contentDirPath: 'posts',
+  contentDirPath: './posts',
   documentTypes: [Post],
 });
