1f500af11b8cc2d299a21fd305886d2a77be9670
ChuYeonBin <cndusqls98@gmail.com>
2023-09-14 16:55:12 +0900

    chore: tailwind config dark-mode 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.tsx', './components/**/*.tsx'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {},
   },
