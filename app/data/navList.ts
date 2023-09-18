201e96446519c30292f317aa775533f65eb1dc28
ChuYeonBin <cndusqls98@gmail.com>
2023-09-18 17:43:57 +0900

    feat: Navbar에 들어갈 데이터 구현

---
+++ app/data/navList.ts
@@ -0,0 +1,7 @@
+const navList: { title: string; link: string }[] = [
+  { title: '블로그', link: '/blog' },
+  { title: '기록', link: '/log' },
+  { title: '이력서', link: '/resume' },
+];
+
+export default navList;
