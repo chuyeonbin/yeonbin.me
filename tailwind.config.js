7f03d5ec42b7a6814ea333e2776ddd24deccd167
ChuYeonBin <cndusqls98@gmail.com>
2023-09-18 17:39:01 +0900

    chore: tailwind typography 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.tsx', './components/**/*.tsx'],
-  darkMode: ['class', '[data-theme="dark"]'],
+  darkMode: ['class'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
