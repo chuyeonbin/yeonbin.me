2762d53312c09ca41c6c5379840705faa5befab1
ChuYeonBin <cndusqls98@gmail.com>
2023-09-19 13:22:12 +0900

    feat: HomeProfile component background gif 적용

---
+++ app/components/HomeProfile.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomeProfile() {
+  return (
+    <div className='rounded-lg bg-no-repeat bg-cover bg-[url(../public/gif/light.gif)] dark:bg-[url(../public/gif/dark.gif)]'>
+      HomeProfile
+    </div>
+  );
+}
