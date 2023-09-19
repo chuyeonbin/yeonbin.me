9bb149bc17afb60a89fa795b7e70ddf94c56f35a
ChuYeonBin <cndusqls98@gmail.com>
2023-09-19 12:04:08 +0900

    style: header component tailwind styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,21 @@
 'use client';
 import Navbar from '@/components/Navbar';
 import ThemeSwitch from './ThemeSwitch';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-40'>
+    <header className='sticky top-0 z-40 bg-white dark:bg-transparent dark:border-b dark:backdrop-blur dark:border-slate-300/10'>
       <div className='container max-w-screen-md mx-auto px-8 py-4'>
         <div className='flex items-center justify-between'>
-          <div>logo</div>
+          <div>
+            <Link href='/'>
+              <h1 className='text-xl md:text-2xl font-bold'>
+                yeonbin
+                <span className='text-indigo-700'>.dev</span>
+              </h1>
+            </Link>
+          </div>
           <div className='flex items-center'>
             <Navbar />
             <ThemeSwitch />
