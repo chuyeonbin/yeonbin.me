43d30b09f466755823c334031bf433bc8db2f063
ChuYeonBin <cndusqls98@gmail.com>
2023-09-21 14:00:13 +0900

    fix: eslint error fix

--- app/components/RecentBlog.tsx
+++ app/components/RecentBlog.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Blog } from 'contentlayer/generated';
-import { dateFormat } from 'app/util/date';
+import dateFormat from 'app/util/date';
 
 interface RecentBlogProps {
   blog: Blog;
@@ -11,11 +11,11 @@ export default function RecentBlog({ blog }: RecentBlogProps) {
     <li className='p-3 border-b dark:border-slate-300/20'>
       <Link href={`blog/${blog.slug}`} className='group'>
         <div className='min-h-min md:grid grid-cols-12 gap-x-4'>
-          <img
-            src='test'
+          {/* <img
+            src={'./public/next.svg'}
             alt='thumbnail'
             className='hidden md:block col-span-3 rounded-lg bg-slate-500'
-          />
+          /> */}
           <div className='col-span-9'>
             <span className='text-slate-500 text-sm md:text-base'>
               {dateFormat(blog.publishAt)}
@@ -27,14 +27,12 @@ export default function RecentBlog({ blog }: RecentBlogProps) {
               {blog.description}
             </p>
             {blog.tags.length > 0 ? (
-              <ul className='mt-5 flex flex-wrap space-x-2 text-xs md:text-sm  text-slate-500/60'>
-                {blog.tags.map((tag) => {
-                  return (
-                    <li key={tag}>
-                      #<span>{tag}</span>
-                    </li>
-                  );
-                })}
+              <ul className='mt-5 flex flex-wrap text-xs md:text-sm  text-slate-500/60'>
+                {blog.tags.map((tag) => (
+                  <li className='mr-4' key={tag}>
+                    #<span>{tag}</span>
+                  </li>
+                ))}
               </ul>
             ) : null}
           </div>
--- app/components/RecentLog.tsx
+++ app/components/RecentLog.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Log } from 'contentlayer/generated';
-import { dateFormat } from 'app/util/date';
+import dateFormat from 'app/util/date';
 
 interface RecentLogProps {
   log: Log;
@@ -9,22 +9,20 @@ interface RecentLogProps {
 export default function RecentLog({ log }: RecentLogProps) {
   return (
     <li className='p-3 border-b dark:border-slate-300/20'>
-      <Link href={`blog/${log.slug}`} className='group'>
-        <div className=''>
+      <Link href={`${log.slug}`} className='group'>
+        <div>
           <div>
             <span className='text-slate-500 text-sm md:text-base'>{dateFormat(log.publishAt)}</span>
             <h3 className='mt-2 text-lg md:text-xl font-semibold group-hover:text-indigo-700 dark:group-hover:text-indigo-300 line-clamp-1'>
               {log.title}
             </h3>
             {log.tags.length > 0 ? (
-              <ul className='mt-5 flex flex-wrap space-x-2 text-xs md:text-sm  text-slate-500/60'>
-                {log.tags.map((tag) => {
-                  return (
-                    <li key={tag}>
-                      #<span>{tag}</span>
-                    </li>
-                  );
-                })}
+              <ul className='mt-5 flex flex-wrap  text-xs md:text-sm  text-slate-500/60'>
+                {log.tags.map((tag) => (
+                  <li className='mr-4' key={tag}>
+                    #<span>{tag}</span>
+                  </li>
+                ))}
               </ul>
             ) : null}
             <p className='mt-2 text-sm md:text-base font-semibold text-slate-400 line-clamp-3'>
