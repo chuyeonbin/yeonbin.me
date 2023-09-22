d74c4b948aa19679c6beb9d128812a9d1494d6f3
ChuYeonBin <cndusqls98@gmail.com>
2023-09-22 15:03:10 +0900

    feat: ScrollTopButton component 구현

---
+++ app/components/ScrollTopButton.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { ChevronUpIcon } from '@heroicons/react/24/solid';
+import { useEffect, useRef } from 'react';
+
+export default function ScrollTopButton() {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    const handleShowButton = () => {
+      if (!buttonRef.current) return;
+
+      if (window.scrollY > 2500) {
+        buttonRef.current.classList.remove('hide');
+      } else {
+        buttonRef.current.classList.add('hide');
+      }
+    };
+    window.addEventListener('scroll', handleShowButton);
+    return () => {
+      window.removeEventListener('scroll', handleShowButton);
+    };
+  }, []);
+
+  return (
+    <button
+      className='fixed bottom-5 right-5 p-2 text-slate-400 hover:text-indigo-300 hover:bg-slate-300/20 rounded-full'
+      onClick={scrollToTop}
+      ref={buttonRef}
+    >
+      <ChevronUpIcon
+        className='w-4 h-4 md:w-6 md:h-6 font-black'
+        viewBox='0 0 24 24'
+        stroke='currentColor'
+        strokeWidth='1'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </button>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { pretendard, tossface } from '@/styles/fonts';
 import '@/styles/global.css';
 import Header from '@/components/Header';
 import ThemeProviders from './ThemeProviders';
+import ScrollTopButton from './components/ScrollTopButton';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Header />
           <main className='container max-w-screen-md mx-auto px-4 py-12 md:py-16'>{children}</main>
           {/* footer */}
+          <ScrollTopButton />
         </ThemeProviders>
       </body>
     </html>
