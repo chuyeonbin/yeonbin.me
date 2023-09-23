1c8493592c7a749a413cbd05baab8c03562e12dc
ChuYeonBin <cndusqls98@gmail.com>
2023-09-23 14:09:15 +0900

    style: MDXPost component styling

--- app/components/MDXPost.tsx
+++ app/components/MDXPost.tsx
@@ -13,7 +13,7 @@ export default function MDXPost({ code }: MDXPostProps) {
   const MDXContent = useMDXComponent(code);
 
   return (
-    <div className='prose'>
+    <div className='prose dark:prose-invert max-w-none  prose-a:text-indigo-400 prose-a:underline-offset-4 prose-a:decoration-2 hover:prose-a:text-indigo-600 prose-code:text-indigo-600  prose-code:before:content-[] prose-code:after:content-[]'>
       <MDXContent components={CustomMdxComponents} />
     </div>
   );
