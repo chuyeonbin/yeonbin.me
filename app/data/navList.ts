11597a10b1d4680147f1851d313f7e9ade0acf9e
ChuYeonBin <cndusqls98@gmail.com>
2023-10-02 19:32:22 +0900

    fix: log link 수정

--- app/data/navList.ts
+++ app/data/navList.ts
@@ -1,6 +1,6 @@
 const navList: { title: string; link: string }[] = [
   { title: '블로그', link: '/blog' },
-  { title: '기록', link: '/log' },
+  { title: '기록', link: '/record' },
   { title: '이력서', link: '/resume' },
 ];
 
