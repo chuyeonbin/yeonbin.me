d563c9fe67eeadd08e29b0d0a288c6a3d0af8c69
ChuYeonBin <cndusqls98@gmail.com>
2023-10-02 20:04:02 +0900

    feat: blog  소개글 작성

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,10 +7,16 @@ export default function Blog() {
 
   return (
     <div>
-      <h1 className='text-xl md:text-2xl font-semibold'>
-        All Posts <span className='text-base'>({blogs.length})</span>
-      </h1>
-      <ul className='mt-12'>
+      <div className='mb-16'>
+        <h1 className='text-xl md:text-2xl font-semibold'>
+          All Posts <span className='text-base'>({blogs.length})</span>
+        </h1>
+        <p className='mt-2 text-sm md:text-base text-slate-500 dark:text-slate-300'>
+          공부와 프로젝트를 진행하면서 겪었던 문제들, 그리고 다양한 지식을 공유하기 위해서 작성하는
+          블로그 입니다. 📖📚🔥
+        </p>
+      </div>
+      <ul>
         {blogs.map((blog) => (
           <BlogCard key={blog._id} blog={blog} />
         ))}
