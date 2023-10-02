01d9981883b5ee542eaaef0136efa89c47fa8c67
ChuYeonBin <cndusqls98@gmail.com>
2023-10-02 18:18:44 +0900

    feat: blog  page 구현

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,20 @@
+import BlogCard from '@/components/BlogCard';
+import { allBlogs } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
+
 export default function Blog() {
-  return <h1>Hello, Dashboard Page!</h1>;
+  const blogs = allBlogs.sort((a, b) => compareDesc(new Date(a.publishAt), new Date(b.publishAt)));
+
+  return (
+    <div>
+      <h1 className='text-xl md:text-2xl font-semibold'>
+        All Posts <span className='text-base'>({blogs.length})</span>
+      </h1>
+      <ul className='mt-12'>
+        {blogs.map((blog) => (
+          <BlogCard key={blog._id} blog={blog} />
+        ))}
+      </ul>
+    </div>
+  );
 }
