8d50323794dbba0c6cb8a0d9e5aa0bab3f501d19
ChuYeonBin <cndusqls98@gmail.com>
2023-10-12 15:19:06 +0900

    style: global style 수정

--- app/components/MDXPost.tsx
+++ app/components/MDXPost.tsx
@@ -8,7 +8,7 @@ export default function MDXPost({ code }: MDXPostProps) {
   const MDXContent = useMDXComponent(code);
 
   return (
-    <div className='prose dark:prose-invert max-w-none  prose-a:text-indigo-400 prose-a:underline-offset-4 prose-a:decoration-2 hover:prose-a:text-indigo-600 prose-code:text-indigo-600 prose-img:mx-auto prose-img:rounded-lg  prose-pre:mt-0 '>
+    <div className='prose dark:prose-invert max-w-none  prose-a:text-indigo-400 prose-a:underline-offset-4 prose-a:decoration-2 hover:prose-a:text-indigo-600 prose-code:text-indigo-600  prose-img:rounded-lg  prose-pre:mt-0 '>
       <MDXContent />
     </div>
   );
