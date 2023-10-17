f592b9d1bd6733d0e7b7aba7332bfc7b1bb6152e
ChuYeonBin <cndusqls98@gmail.com>
2023-10-17 12:53:42 +0900

    feat: useIntersectionObserver 커스텀 훅 구현

---
+++ app/hooks/useIntersectionObserver.ts
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction, useCallback, useEffect } from 'react';
+
+export default function useIntersectionObserver(setState: Dispatch<SetStateAction<string>>) {
+  const observerCallback: IntersectionObserverCallback = useCallback((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && entry.intersectionRatio > 0) {
+        setState(entry.target.id);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      threshold: 0.4,
+      rootMargin: '-76px 0px 0px 0px',
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    const headings = Array.from(document.querySelectorAll('h2, h3, h4'));
+
+    headings.forEach((heading) => observer.observe(heading));
+
+    return () => observer && observer.disconnect();
+  }, []);
+}
