2dcd9d18b9abeae74c75f500cd4df4b9fca37769
ChuYeonBin <cndusqls98@gmail.com>
2023-11-21 14:07:04 +0900

    fix: navList data 수정

--- app/data/navList.ts
+++ app/data/navList.ts
@@ -2,7 +2,7 @@ const navList: { title: string; link: string }[] = [
   { title: '블로그', link: '/blog' },
   { title: '노트', link: '/note' },
   { title: '프로젝트', link: '/projects' },
-  { title: '이력서', link: '/resume' },
+  { title: '소개', link: '/about' },
 ];
 
 export default navList;
